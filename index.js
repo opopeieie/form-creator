81f5a2379f3c8f65766b6d13896cdc08880cefa4
Oscar.chen <ChenXinqiao@chinasofti.com>
2017-07-05 09:08:44 +0800

    add form-creator p

---
+++ index.js
@@ -0,0 +1,4 @@
+const cheerio = require('cheerio'),
+      fs = require('fs');
+
+fs.writeFileSync('./index.html','<html></html>');
---
+++ server.js
@@ -0,0 +1,74 @@
+var mine = {
+    "css": "text/css",
+    "gif": "image/gif",
+    "html": "text/html",
+    "ico": "image/x-icon",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "js": "text/javascript",
+    "json": "application/json",
+    "pdf": "application/pdf",
+    "png": "image/png",
+    "svg": "image/svg+xml",
+    "swf": "application/x-shockwave-flash",
+    "tiff": "image/tiff",
+    "txt": "text/plain",
+    "wav": "audio/x-wav",
+    "wma": "audio/x-ms-wma",
+    "wmv": "video/x-ms-wmv",
+    "xml": "text/xml"
+}
+
+
+var PORT = 8009;
+
+var http = require('http');
+var url=require('url');
+var fs=require('fs');
+var path=require('path');
+
+var server = http.createServer(function (request, response) {
+    var pathname = request.url;
+    var realPath;
+    if(pathname === '/'){
+        realPath = 'index.html'
+    }else{
+        realPath = "./" + url.parse(request.url).pathname;
+    }
+    console.log(realPath);
+    var ext = path.extname(realPath);
+    ext = ext ? ext.slice(1) : 'unknown';
+    fs.exists(realPath, function (exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+
+            response.write("This request URL " + pathname + " was not found on this server.");
+            response.end();
+        } else {
+            fs.readFile(realPath, "binary", function (err, file) {
+                if (err) {
+                    response.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    response.end();
+                } else {
+                    var contentType = mine[ext]+";charset=utf-8" || "text/html;charset=utf-8";
+                    response.writeHead(200, {
+                        'Content-Type': contentType
+                    });
+                    response.write(file, "binary");
+                    response.end();
+                }
+            });
+            // var contentType = mine[ext] || "text/plain";
+            // response.writeHead(200, {
+            //     'Content-Type': contentType
+            // });
+            // response.end();
+        }
+    });
+});
+server.listen(PORT);
+console.log("Server runing at port: " + PORT + ".");
